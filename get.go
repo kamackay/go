86bf67bcc178b47534ea9440449706e48cc519cc
Keith MacKay <Keith.MacKay@Honeywell.com>
2017-07-13 14:19:06 -0400

    better logging again

--- get.go
+++ get.go
@@ -10,18 +10,23 @@ import (
 
 func main() {
 	args := os.Args[1:]
+	// Just to make the call to log shorter
 	log := func(s ...interface{}) { fmt.Println(s...) }
 
 	url := ""
 	if len(args) > 0 {
 		url = args[0]
+	} else {
+		log("Please input a URL to GET")
+		return
 	}
 	log("GET on", url, "\n")
 	startTime := time.Now()
 	resp, err := http.Get(url)
 
 	completedTime := time.Now().Sub(startTime)
-	defer log("GET took", completedTime.String())
+	// show the user how long it took for the server to respond
+	defer log("\nGET on", url, "took", completedTime.String())
 
 	if err != nil {
 		log("There was an error in the GET")
