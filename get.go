cd5c97a3fc0ce49ec795b16860c6a0b98185fb0f
Keith MacKay <Keith.MacKay@Honeywell.com>
2017-07-13 14:52:33 -0400

    nevermind on that last commit

--- get.go
+++ get.go
@@ -22,12 +22,6 @@ func main() {
 	}
 	log("GET on", url, "\n")
 	startTime := time.Now()
-	go func() {
-		time.Sleep(2)
-		log("Timed Out")
-		os.Exit(1)
-	}()
-
 	resp, err := http.Get(url)
 
 	completedTime := time.Now().Sub(startTime)
