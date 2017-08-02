e6d9a2d837713ee1447bc7bbafb1881e48d82016
Keith MacKay <Keith.MacKay@Honeywell.com>
2017-08-02 11:48:09 -0400

    added second endpoint

--- server.go
+++ server.go
@@ -10,5 +10,10 @@ func main() {
 		defer println("Connection")
 		io.WriteString(w, "<h1>Hello world</h1><h2>Ver2</h2>")
 	})
+	http.HandleFunc("/page/", func(w http.ResponseWriter, r *http.Request) {
+		defer println("Connection")
+		io.WriteString(w, "<h1>This is a page!</h1>")
+	})
+	go println("Listening on port 7000")
 	http.ListenAndServe(":7000", nil)
 }
