969ccacce65e55b9a68fc98bb40f407c6a46785a
Keith MacKay <Keith.MacKay@Honeywell.com>
2017-08-02 14:21:24 -0400

    using port const now

--- server.go
+++ server.go
@@ -3,9 +3,12 @@ package main
 import (
 	"io"
 	"net/http"
+	"fmt"
 )
 
 func main() {
+	const port = 7000
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		defer println("Connection")
 		io.WriteString(w, "<h1>Hello world</h1><h2>Ver2</h2>")
@@ -14,6 +17,6 @@ func main() {
 		defer println("Connection")
 		io.WriteString(w, "<h1>This is a page!</h1>")
 	})
-	go println("Listening on port 7000")
-	http.ListenAndServe(":7000", nil)
+	go println(fmt.Sprintf("Listening on port %d", port))
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
