03d1c7047d8e7cd494cbd39b7fc12fe3e8cb88b5
Keith MacKay <Keith.MacKay@Honeywell.com>
2017-08-09 15:47:23 -0400

    damn it's fast

--- findTabs.go
+++ findTabs.go
@@ -5,7 +5,18 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"regexp"
+	"time"
 )
+
+func in(a byte, list []byte) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
 
 func getFiles(dir string) []string {
 	var files []string
@@ -25,13 +36,36 @@ func getFiles(dir string) []string {
 func main() {
 	log := func(s ...interface{}) { fmt.Println(s...) }
 	args := os.Args[1:]
+	startTime := time.Now()
 	dir, _ := os.Getwd()
+	match := ".*py$"
 	if len(args) > 0 {
 		dir = args[0]
 	}
+	if len(args) > 1 {
+		match = args[1]
+	}
+
+	numFiles := 0
 
 	log("Searching for tabs in", dir)
 	for _, f := range getFiles(dir) {
-		log(f)
+		r, err := regexp.MatchString(match, f)
+		if r && err == nil {
+			data, err := ioutil.ReadFile(f)
+			if err != nil {
+				log(err)
+			} else {
+				if in(byte('\t'), data) {
+					log("Tab in", f)
+					numFiles++
+				}
+			}
+		}
 	}
+	log("")
+	log(numFiles, "files had tabs")
+	completedTime := time.Now().Sub(startTime)
+	// show the user how long it took for the server to respond
+	defer log("\n", completedTime.String())
 }
